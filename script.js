850a16f0d5a7c7c2c0f2135f77035a4bbeeb4c7e
Csontos <csontosdorina>
2023-10-24 11:22:46 +0200

    készen van

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const button = document.querySelector("button");
 button.addEventListener("click", createResult);
 
+const type  = {
+    "adult" : 350,
+    "student":175,
+    "retired": 100,
+    "pet":75,
+
+}
+
 function createResult(){
     const quantity = document.querySelector(".myContainer div input");
     const ticketType = document.querySelector(".myContainer div:nth-child(2) select");
@@ -16,7 +24,19 @@ function createResult(){
     */
     /*Ternary operator: kifejezés ? Ha igaz : Ha hamis */
     discount = quantity.value >= 10 ? 0.9 : 1;
-    result.value = ticketType.value === "adult" ? `${quantity.value*350 * discount} Ft` : `${quantity.value * 175 * discount} Ft`;
-    
+    /*result.value = ticketType.value === "adult" ? `${quantity.value*350 * discount} Ft` : `${quantity.value * 175 * discount} Ft`;
+    */
+    if(
+ticketType.value === "pet" && quantity.value > 1){
+    quantity.value= "";
+    quantity.focus();
+    discount=1;
+    alert("csak egy allatozt vihec")
 
+
+    }
+
+//AMIKOR TÖBB ÁLLATOT ÍRNAK BE AKKOR QUANTITY.VALUE ÉRTÉKE ÜRES SZTRING    lesz
+//megis mukodik a szorzas itt lenn mert "" --> 0 lesz inplicit konverzio 
+    result.value = quantity.value * discount * type[ticketType.value];
 }
